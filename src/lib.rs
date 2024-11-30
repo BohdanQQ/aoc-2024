6cadd1469cc962a9dbaa2829ecabe87bf1bbf8e2
BohdanQQ <40754203+BohdanQQ@users.noreply.github.com>
2024-11-30 20:35:23 +0000

    add regex lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
 pub mod template;
+use regex::Regex;
 
 // Use this file to add helper functions and additional modules.
+
+pub fn matches(pattern: &str, haystack: &str) -> bool {
+   Regex::new(pattern).unwrap().is_match(haystack)
+}
+
+pub fn capture_groups<'h>(pattern: &str, haystack: &'h str) -> Option<regex::Captures<'h>> {
+  Regex::new(pattern).unwrap().captures(haystack)
+}
