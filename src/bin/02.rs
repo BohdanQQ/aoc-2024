592c99b2d808ecd8be00b85514685e659f8b6830
BohdanQQ <40754203+BohdanQQ@users.noreply.github.com>
2025-01-02 15:35:22 +0100

    simplify and speed up d2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -9,9 +9,15 @@ enum SafetyStatus {
     UnininitFirst(u32),
 }
 
-fn get_safety(seq: &Vec<u32>, tolerate: bool, skip_idx: Option<usize>) -> SafetyStatus {
+fn get_safety(
+    seq: impl Iterator<Item = u32> + Clone,
+    tolerate: bool,
+    skip_idx: Option<usize>,
+) -> SafetyStatus {
     let mut idx = 0;
-    let result = seq.iter().fold(SafetyStatus::Unininit, |acc, next| {
+    let mut count = 0;
+    let result = seq.clone().fold(SafetyStatus::Unininit, |acc, next| {
+        count += 1;
         if let Some(i) = skip_idx {
             if idx == i {
                 idx += 1;
@@ -20,23 +26,24 @@ fn get_safety(seq: &Vec<u32>, tolerate: bool, skip_idx: Option<usize>) -> Safety
         }
         idx += 1;
         match acc {
+            // using ops::ControlFlow does not help
             SafetyStatus::Unsafe => acc,
-            SafetyStatus::Unininit => SafetyStatus::UnininitFirst(*next),
-            SafetyStatus::UnininitFirst(last) if last < *next && last.abs_diff(*next) < 4 => {
-                SafetyStatus::SafeIncreasing(*next)
+            SafetyStatus::Unininit => SafetyStatus::UnininitFirst(next),
+            SafetyStatus::UnininitFirst(last) if last < next && last.abs_diff(next) < 4 => {
+                SafetyStatus::SafeIncreasing(next)
             }
-            SafetyStatus::UnininitFirst(last) if last > *next && last.abs_diff(*next) < 4 => {
-                SafetyStatus::SafeDecreasing(*next)
+            SafetyStatus::UnininitFirst(last) if last > next && last.abs_diff(next) < 4 => {
+                SafetyStatus::SafeDecreasing(next)
             }
             SafetyStatus::UnininitFirst(_) => SafetyStatus::Unsafe,
-            SafetyStatus::SafeDecreasing(last) if *next >= last || last.abs_diff(*next) >= 4 => {
+            SafetyStatus::SafeDecreasing(last) if next >= last || last.abs_diff(next) >= 4 => {
                 SafetyStatus::Unsafe
             }
-            SafetyStatus::SafeDecreasing(_) => SafetyStatus::SafeDecreasing(*next),
-            SafetyStatus::SafeIncreasing(last) if *next <= last || last.abs_diff(*next) >= 4 => {
+            SafetyStatus::SafeDecreasing(_) => SafetyStatus::SafeDecreasing(next),
+            SafetyStatus::SafeIncreasing(last) if next <= last || last.abs_diff(next) >= 4 => {
                 SafetyStatus::Unsafe
             }
-            SafetyStatus::SafeIncreasing(_) => SafetyStatus::SafeIncreasing(*next),
+            SafetyStatus::SafeIncreasing(_) => SafetyStatus::SafeIncreasing(next),
         }
     });
     if matches!(result, SafetyStatus::Unsafe) && tolerate {
@@ -45,8 +52,8 @@ fn get_safety(seq: &Vec<u32>, tolerate: bool, skip_idx: Option<usize>) -> Safety
         // 2-step backtrack, making the state even bigger...)
         // sequences are very short anyway
         // or you could try skipping idx_of_failure - 1, idx_of_failure, and idx_of_failure +1
-        for idx in 0..seq.len() {
-            let res = get_safety(seq, false, Some(idx));
+        for idx in 0..count {
+            let res = get_safety(seq.clone(), false, Some(idx));
             if matches!(res, SafetyStatus::SafeDecreasing(_))
                 || matches!(res, SafetyStatus::SafeIncreasing(_))
             {
@@ -59,21 +66,15 @@ fn get_safety(seq: &Vec<u32>, tolerate: bool, skip_idx: Option<usize>) -> Safety
 
 // could be like 10% faster if I was able to write the iterator return time
 // (avoiding the final collect call)
-pub fn sequences(input: &str) -> Vec<Vec<u32>> {
-    input
-        .split('\n')
-        .filter(|l| !l.trim().is_empty())
-        .map(|l| {
-            l.split_ascii_whitespace()
-                .map(|v| v.parse::<u32>().unwrap())
-                .collect::<Vec<_>>()
-        })
-        .collect()
+pub fn sequences(input: &str) -> impl Iterator<Item = impl Iterator<Item = u32> + Clone + '_> + '_ {
+    input.lines().map(|l| {
+        l.split_ascii_whitespace()
+            .map(|v| v.parse::<u32>().unwrap())
+    })
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
     let correct_count = sequences(input)
-        .iter()
         .map(|seq| get_safety(seq, false, None))
         .filter(|s| !matches!(s, SafetyStatus::Unsafe))
         .count();
@@ -82,7 +83,8 @@ pub fn part_one(input: &str) -> Option<usize> {
 
 pub fn part_two(input: &str) -> Option<usize> {
     let correct_count = sequences(input)
-        .iter()
+        // part 2 50us faster with seq.collect (i.e. slice, not an iterator)
+        // but p1 became 100us faster => gain of 50us
         .map(|seq| get_safety(seq, true, None))
         .filter(|s| !matches!(s, SafetyStatus::Unsafe))
         .count();
