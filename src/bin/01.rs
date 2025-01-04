712cbd726ee2dcdd4820ef1288922e01767c5cee
BohdanQQ <40754203+BohdanQQ@users.noreply.github.com>
2025-01-04 20:20:08 +0100

    speed up day1 by 50%

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -4,13 +4,13 @@ advent_of_code::solution!(1);
 
 fn parse(input: &str) -> (Vec<u32>, Vec<u32>) {
     let mut res = (vec![], vec![]);
+    // this gets another few % faster if rewritten in a for loop
     input
         .lines()
-        .map(|v| v.split_ascii_whitespace().collect::<Vec<_>>())
-        .map(|a| (a[0].parse().unwrap(), a[1].parse().unwrap()))
-        .for_each(|(o, t)| {
-            res.0.push(o);
-            res.1.push(t);
+        .for_each(|line| {
+            let mut it = line.split_ascii_whitespace();
+            res.0.push(it.next().unwrap().parse().unwrap());
+            res.1.push(it.next().unwrap().parse().unwrap());
         });
     res
 }
@@ -19,6 +19,8 @@ pub fn part_one(input: &str) -> Option<u32> {
     let (mut v1, mut v2) = parse(input);
     v1.sort();
     v2.sort();
+    // keeping the zip and rewriting the rest with a loop results in a miniscule
+    // but present uplift
     v1.iter()
         .zip(v2)
         .fold(0, |acc, (one, two)| acc + one.abs_diff(two))
@@ -29,17 +31,14 @@ pub fn part_two(input: &str) -> Option<u64> {
     let (v1, v2) = parse(input);
     let right_set: HashSet<u32> = std::collections::HashSet::from_iter(v2.clone());
     let mut right_map = std::collections::HashMap::new();
-    v2.iter().for_each(|v| {
-        if !right_map.contains_key(v) {
-            right_map.insert(v, 0);
-        }
-        if right_set.contains(v) {
-            right_map.insert(v, right_map.get(v).unwrap() + 1);
-        }
+    v2.iter().filter(|v| right_set.contains(v)).for_each(|v| {
+        right_map.entry(v).and_modify(|v| *v += 1).or_insert(1);
     });
     let mut res = 0u64;
-    for v in v1.iter() {
-        res += *v as u64 * *right_map.get(&v).unwrap_or(&0u64);
+    // curiously, writing this as a fold slows down part 2 by >10%
+    // using filter better than unwrap or
+    for v in v1.iter().filter(|v| right_map.contains_key(v)) {
+        res += *v as u64 * *right_map.get(&v).unwrap();
     }
     Some(res)
 }
